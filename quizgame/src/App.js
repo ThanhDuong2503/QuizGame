1b1c05af1982b2e07c4b5754e16f8a51b46e4bc8
ThanhDuong2503 <thanh.dn2503@gmail.com>
2020-09-07 18:19:55 +0200

    add basic structure

--- quizgame/src/App.js
+++ quizgame/src/App.js
@@ -2,23 +2,44 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>hiho there</h1>
   );
 }
+
+const SampleFlashCards = [
+  {
+    id: 1,
+    question: "2+2?",
+    answer: "4",
+    options: [
+        "2",
+        "3",
+        "4",
+        "5"
+    ]
+  },
+    {
+        id: 2,
+        question: "3+3?",
+        answer: "6",
+        options: [
+            "6",
+            "3",
+            "4",
+            "5"
+        ]
+    },
+    {
+        id: 3,
+        question: "4+4?",
+        answer: "8",
+        options: [
+            "8",
+            "3",
+            "4",
+            "5"
+        ]
+    }
+]
 
 export default App;
