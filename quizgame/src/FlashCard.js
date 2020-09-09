c1539f57c879c408da692996e9bbd005140503c2
ThanhDuong2503 <thanh.dn2503@gmail.com>
2020-09-09 18:24:27 +0200

    add responsive auto resizing cards

--- quizgame/src/FlashCard.js
+++ quizgame/src/FlashCard.js
@@ -1,13 +1,33 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useRef} from "react";
 
 function FlashCard({flashcard}) {
 
     const [flip, setFlip] = useState(false);
+    const [height, setHeight] = useState("initial");
+
+    const frontEl = useRef();
+    const backEl = useRef();
+
+    function setMaxHeight() {
+        const frontHeight = frontEl.current.getBoundingClientRect().height;
+        const backHeight = backEl.current.getBoundingClientRect().height;
+        setHeight(Math.max(frontHeight, backHeight, 100));
+    }
+
+    useEffect(() => {
+        setMaxHeight()
+    }, [flashcard.question, flashcard.answer, flashcard.options]);
+
+    useEffect(() => {
+        window.addEventListener("resize", setMaxHeight);
+        return () => window.removeEventListener("resize", setMaxHeight);
+    }, []);
 
     return (
         <div className={`card ${flip ? 'flip' : ''}`}
+             style={{height: height}}
              onClick={() => setFlip(!flip)}>
-            <div className="front">
+            <div className="front" ref={frontEl}>
                 {flashcard.question}
                 <div className="flashcard-options">
                     {flashcard.options.map(option => {
@@ -17,7 +37,7 @@ function FlashCard({flashcard}) {
                     })}
                 </div>
             </div>
-            <div className="back">{flashcard.answer}</div>
+            <div className="back" ref={backEl}>{flashcard.answer}</div>
         </div>
     )
 }
